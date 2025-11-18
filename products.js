ab8bef876f389f6f6306d104f275d9c7ec22e652
chris-techlite <chilurunkachineduokoro@gmail.com>
2025-11-18 16:23:40 +0400

    repo creation for front end

---
+++ products.js
@@ -0,0 +1,159 @@
+let products = [
+                
+                {
+                    "id": 2000,
+                    "title": "KARATE",
+                    "description": "Self defense class for all ages",
+                    "price": 5000,
+                    "image": 'img/karate.jpg',
+                    "availableInventory": 5,
+                    "location": "B15-1-09",
+                    "rating": 4
+
+                },
+                {
+                    "id": 3000,
+                    "title": "MUSIC",
+                    "description": "Learn to play an instrument of your choice",
+                    "price": 5000,
+                    "image": 'img/music.jpg',
+                    "availableInventory": 5,
+                    "location": "B15-1-10",
+                    "rating": 5
+
+                },
+                 {
+                    "id": 4000,
+                    "title": "BASKETBALL",
+                    "description": "Learn to play basketball like a pro",
+                    "price": 5000,
+                    "image": 'img/basketball.png',
+                    "availableInventory": 5,
+                    "location": "B15-1-11",
+                    "rating": 4
+
+                },
+                 {
+                    "id": 5000,
+                    "title": "ART AND CRAFT",
+                    "description": "Express yourself with colors and creativity",
+                    "price": 5000,
+                    "image": 'img/artandcraft.jpg',
+                    "availableInventory": 5,
+                    "location": "B15-1-12",
+                    "rating": 5
+
+                },
+                 {
+                    "id": 6000,
+                    "title": "ROBOTICS",
+                    "description": "Learn to build and program robots",
+                    "price": 5000,
+                    "image": 'img/robotics.png',
+                    "availableInventory": 5,
+                    "location": "B15-1-13",
+                    "rating": 3
+
+                },
+                 {
+                    "id": 7000,
+                    "title": "POETRY",
+                    "description": "Unleash the power of words",
+                    "price": 5000,
+                    "image": 'img/poetry.jpeg',
+                    "availableInventory": 5,
+                    "location": "B15-1-14",
+                    "rating": 4
+
+                },
+                 {
+                    "id": 8000,
+                    "title": "CHESS",
+                    "description": "Learn the game of kings",
+                    "price": 5000,
+                    "image": 'img/chess.png',
+                    "availableInventory": 5,
+                    "location": "B15-1-15",
+                    "rating": 5
+
+                },
+                 {
+                    "id": 9000,
+                    "title": "CULINARY",
+                    "description": "Express your creativity with food",
+                    "price": 5000,
+                    "image": 'img/CULINARY.jpeg',
+                    "availableInventory": 5,
+                    "location": "B15-1-16",
+                    "rating": 4
+
+                },
+                 {
+                    "id": 10000,
+                    "title": "FASHION",
+                    "description": "Learn to design and sew your own clothes",
+                    "price": 5000,
+                    "image": 'img/fashion.png',
+                    "availableInventory": 5,
+                    "location": "B15-1-17",
+                    "rating": 5
+
+                },
+                 {
+                    "id": 11000,
+                    "title": "PHOTOGRAPHY",
+                    "description": "Capture the world through your lens",
+                    "price": 5000,
+                    "image": 'img/photography.png',
+                    "availableInventory": 5,
+                    "location": "B15-1-18",
+                    "rating": 4
+
+                },
+
+                 {
+                    "id": 12000,
+                    "title": "VOLLEYBALL",
+                    "description": "Learn to play volleyball like a pro",
+                    "price": 5000,
+                    "image": 'img/volleyball.jpeg',
+                    "availableInventory": 5,
+                    "location": "B15-1-19",
+                    "rating": 3
+
+                },
+               {
+                    "id": 13000,
+                    "title": "FOOTBALL",
+                    "description": "Learn to play football like a pro",
+                    "price": 5000,
+                    "image": 'img/football.jpeg',
+                    "availableInventory": 5,
+                    "location": "B15-1-20",
+                    "rating": 5
+                },
+               {
+                    "id": 14000,
+                    "title": "GAMING",
+                    "description": "Do what you love",
+                    "price": 5000,
+                    "image": 'img/gaming.png',
+                    "availableInventory": 5,
+                    "location": "B15-1-21",
+                    "rating": 3
+
+                },
+                {
+                    "id": 15000,
+                    "title": "GARDENING",
+                    "description": "Learn to grow your own food",
+                    "price": 5000,
+                    "image": 'img/garden.jpg',
+                    "availableInventory": 5,
+                    "location": "B15-1-22",
+                    "rating": 5
+
+                }
+
+            ]
+
